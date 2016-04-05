60e5d969ffc0969dd192de6705caa39473f85bd0
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-04-05 16:51:31 -0400

    Add current time exercise

--- first_sinatra/app.rb
+++ first_sinatra/app.rb
@@ -8,6 +8,11 @@ get "/about" do
   erb :about
 end
 
+get "/current_time" do
+  @t = Time.now
+  erb :current_time
+end
+
 get "/pizza" do
   @ingredients = [ "pepperonni", "green peppers", "mushrooms" ]
   erb :pizza
