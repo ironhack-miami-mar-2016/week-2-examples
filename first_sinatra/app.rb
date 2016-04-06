d2769b9543dc879390d953ef7fcaf7c7b3fb2b3e
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-04-06 12:08:45 -0400

    Add sessions and reloader

--- first_sinatra/app.rb
+++ first_sinatra/app.rb
@@ -1,6 +1,11 @@
 require "sinatra"
+require "sinatra/reloader"
+
+enable :sessions
+
 
 get "/" do
+  @session_info = session.inspect
   erb :home
 end
 
@@ -10,10 +15,32 @@ end
 
 get "/current_time" do
   @t = Time.now
+
   erb :current_time
 end
 
 get "/pizza" do
   @ingredients = [ "pepperonni", "green peppers", "mushrooms" ]
   erb :pizza
+end
+
+get "/no_reloading" do
+  "NO RELOADS REQUIRED!!"
+end
+
+get "/tea_time" do
+  status 418
+  "Welcome to tea time! I'm a tea pot."
+end
+
+get "/session_test/:text" do
+  text = params[:text]
+
+  if session[:saved_stuff].nil?
+    session[:saved_stuff] = []
+  end
+
+  session[:saved_stuff].push(text)
+
+  redirect to("/")
 end
